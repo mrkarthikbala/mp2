4c73fefd597e6110a1a02544be4abccb40c48d83
Karthik Bala <kbala2@illinois.edu>
2015-03-05 09:15:50 -0600

    safekeeping

--- js/controllers.js
+++ js/controllers.js
@@ -1,8 +1,18 @@
 var demoApp = angular.module('demoApp', []);
 
-// demoApp.controller('demoController', ['$scope', '$http', function($scope, $http) {
-//   $http.get('public/data/cs498rk.json').success(function(data) {
-//     $scope.courseInfo = data;
-//     $scope.artistOrder = 'name';
-//   });
-// }]);
+demoApp.controller('demoController', ['$scope', '$http', function($scope, $http) {
+  $http.get('public/data/cs498rk.json').success(function(data) {
+    $scope.courseInfo = data;
+    $scope.artistOrder = 'name';
+  });
+}]);
+
+
+var listApp = angular.module("movieListApp", []);
+
+listApp.controller("movieListController", ['$scope', '$http', function($scope, $http){
+	$http.get('data/imdb250.json').success(function(data){
+		$scope.movies = data;
+	});
+}]);
+
