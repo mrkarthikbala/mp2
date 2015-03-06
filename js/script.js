680cb65dd4f004b4b961616d5bb71bb0930d03b1
Karthik Bala <kbala2@illinois.edu>
2015-03-06 10:51:28 -0600

    did most of part a

--- js/script.js
+++ js/script.js
@@ -1,1 +1,22 @@
 $(document).foundation();
+
+$(document).ready(function(){
+	navShrink();
+});
+
+var navShrink = function(){
+  $(window).scroll(function(){
+      if ( $(document).scrollTop() > 50 ){
+      
+        $("bar").css("height", "60px");
+        $("dd").css("font-size", "10px");	
+      }
+      else {
+        $("bar").css("height", "75px");
+        $("dd").css("font-size", "15px");
+
+      }
+
+
+      });
+}
