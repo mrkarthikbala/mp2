b1deefacb8b766be51c0592520152b25caf19c22
Karthik Bala <kbala2@illinois.edu>
2015-03-05 19:54:03 -0600

    finally got asc/desc

--- js/controllers.js
+++ js/controllers.js
@@ -1,7 +1,11 @@
 movieApp.controller("movieListController", ['$scope', '$http', function($scope, $http){
+	
 	$http.get('data/imdb250.json').success(function(data){
 		$scope.movies = data;
 	});
+	
+	$scope.selectedAttr = "rank";
+
 }]);
 
 movieApp.controller("movieDetailController", ['$scope', '$routeParams', '$http', function($scope, $routeParams, $http){
